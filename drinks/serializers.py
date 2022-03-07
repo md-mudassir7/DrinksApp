f657ce73a3de17c7076b2880dcbd325f21131d75
mohammedmudassirs <mohammedmudassir200@gmail.com>
2022-03-07 11:24:16 +0530

    Initial commit

---
+++ drinks/serializers.py
@@ -0,0 +1,11 @@
+from dataclasses import field
+import imp
+from rest_framework import serializers
+
+from .models import Drink
+
+class DrinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Drink
+        fields = ['id','name','description']
+        
